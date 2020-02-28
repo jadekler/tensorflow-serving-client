331bbed6033405785c8f3107691b44ace6213dc1
Jean de Klerk <deklerk@google.com>
2020-02-28 15:58:30 -0700

    remove lib.go entry, and run go mod tidy

--- lib.go
+++ lib.go
@@ -1,7 +1,6 @@
 package tfsclient
 
 import (
-  _ "github.com/golang/protobuf/ptypes"
-  _ "google.golang.org/grpc"
-  _ "tensorflow_serving/apis"
+	_ "github.com/golang/protobuf/ptypes"
+	_ "google.golang.org/grpc"
 )
